a312f6e58979bd0b2d5998bb4dbcbc52c796b622
ecdata <hongleishen@hotmail.com>
2017-03-08 16:28:56 +0800

    Initial commit

---
+++ src/main/com/servlet/Test.java
@@ -0,0 +1,10 @@
+package com.servlet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.abs(~2016));
+	}
+
+}
---
+++ src/main/com/servlet/TestServlet.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ */
+public class TestServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String name = (String) request.getParameter("name");
+		System.out.println("name==>" + name);
+		request.setAttribute("name", name);
+		if ("cmp".equals(name)) {
+			request.getRequestDispatcher("success.jsp").forward(request,
+					response);// 转发，是同一个请求，转发到的jsp可以得到request里面的属性值
+		} else {
+			request.setAttribute("msg", "用户名和密码错误！！！");
+			request.getRequestDispatcher("index.jsp").forward(request, response);// 转发，是同一个请求，转发到的jsp可以得到request里面的属性值
+			// response.sendRedirect("index.jsp");//重定向，属于不同的请求，下个jsp无法得到request里面的值
+		}
+	}
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+	}
+
+}
